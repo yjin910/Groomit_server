153df66cc552ac44f998025b55848863d6e23e85
YeonwooSung <neos960518@gmail.com>
2019-09-10 21:45:39 +0900

    Trying to fix errors

--- src/logoneg.js
+++ src/logoneg.js
@@ -8,6 +8,8 @@ router.get('/', (req, res) => {
         var s1 = moment(new Date().toLocaleString('en-GB', { timeZone: 'Asia/Seoul', hour12: false }), 'MM/DD/YYYY, hh:mm:ss');
         var s2 = s1.format('YYYY-MM-DDTHH:mm:ssZ')
 
+        console.log(req.query);
+
         //get datas from req
         var device_uid = req.query.u;
         var g_value = req.query.g;
@@ -17,6 +19,8 @@ router.get('/', (req, res) => {
         if(g_value){
             dbcon.addData(device_uid, "g", g_value, time_val, res);
             res.send("success");
+        } else {
+            res.send('Please provide params');
         }
     } catch (e){
         console.error('catch error: ', e.stack);
