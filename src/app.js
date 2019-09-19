b01e55a06994a159c844dd372c01b089fee6ce42
YeonwooSung <neos960518@gmail.com>
2019-09-20 03:09:24 +0900

    Add new module to use session

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var app = express();
 
 var bodyParser = require('body-parser');
