cd0e12ee59486baa56f57d827f50ddd2f0be91bc
yjin910 <yoojin9160@gmail.com>
2020-01-14 02:20:48 +0900

    Change i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,12 @@
-import I18n from 'react-native-i18n';
-import en from './locale/en';
-import ko from './locale/ko';
+// import I18n from 'react-native-i18n';
+// import en from './locale/en';
+// import ko from './locale/ko';
 
-I18n.fallbacks = true;
+// I18n.fallbacks = true;
 
-I18n.translations = {
-    en,
-    ko
-};
+// I18n.translations = {
+//     en,
+//     ko
+// };
 
-export default I18n;
+// export default I18n;
