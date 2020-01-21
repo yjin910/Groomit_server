b9cc7a1ed5211dfcd8d17c3bc5bdd0b30f5c5b8a
yjin910 <yoojin9160@gmail.com>
2020-01-21 13:33:20 +0900

    Add logonecth.js

---
+++ src/logonecth.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var dbcon = require('./dbConnection');
+var moment = require('moment');
+
+router.get('/', (req, res) => {
+    try{
+        // server: 'MM/DD/YYYY, hh:mm:ss', local: 'DD/MM/YYYY, hh:mm:ss'
+        // If not, moment.invalid error would be occurred
+        var s1 = moment(new Date().toLocaleString('en-GB', { timeZone: 'Asia/Seoul', hour12: false }), 'MM/DD/YYYY, hh:mm:ss');
+        var s2 = s1.format('YYYY-MM-DDTHH:mm:ssZ')
+
+        //get datas from req
+        var device_uid = req.query.u;
+        var c_value = req.query.c;
+        var t_value = req.query.t;
+        var h_value =  req.query.h;
+        var time_val = s2;
+        var seq = req.query.s;
+
+        if (c_value) {
+            dbcon.updateData(res, device_uid, "c", c_value, time_val);
+            dbcon.updateData(res, device_uid, "t", t_value, time_val);
+            dbcon.updateData(res, device_uid, "h", h_value, time_val);
+            res.send("success");
+        } else {
+            res.send('Please provide suitable parameters');
+        }
+
+    } catch (e){
+        console.error('catch error: ', e.stack);
+        res.status(404);
+    }
+
+});
+
+module.exports = router;
