98b7931efa1c6a7688e0fb0fc74241cb0c5c1b86
yjin910 <yoojin9160@gmail.com>
2020-01-22 18:32:27 +0900

    Update new parameter

--- src/main.js
+++ src/main.js
@@ -14,14 +14,23 @@ router.get('/', (req, res) => {
     }
 });
 
+// router.get('/graphInfo', (req, res) => {
+//     console.log(req.query.start);
+//     dbcon.getData(res, req.query.u, req.query.type, req.query.start, req.query.end);
+// });
+
 router.get('/graphInfo', (req, res) => {
     console.log(req.query.start);
-    dbcon.getData(res, req.query.u, req.query.type, req.query.start, req.query.end);
+    dbcon.getData(res, req.query.u, req.query.start, req.query.end);
 });
 
 router.get('/mainUuid', (req, res) => {
     dbcon.getUserProfile(res, req.query.email);
 });
+
+router.get('/deviceType', (req, res) =>{
+    dbcon.getDeviceType(res, req.query.uuid);
+})
 
 router.post('/dateLimit', (req, res) => {
     dbcon.getDateLimit(res, req.query.u, req.query.start, req.query.end);
