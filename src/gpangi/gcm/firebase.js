2694aef4fd80c7da36a09863efb9298c2ae8a697
YeonwooSung <neos960518@gmail.com>
2020-06-28 18:02:42 +0900

    Make new directory for GPangi project

--- src/gpangi/gcm/firebase.js
+++ src/gpangi/gcm/firebase.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../../private/gpangi-firebase-adminsdk.json");
+var serviceAccount = require("../../../private/gpangi-firebase-adminsdk.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
