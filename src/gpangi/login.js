57c8496b4f7461a1cfa292357a446e91b1e9f84a
YeonwooSung <neos960518@gmail.com>
2020-06-29 00:05:28 +0900

    Start implementing the log in page for the Gpangi

--- src/gpangi/login.js
+++ src/gpangi/login.js
@@ -3,4 +3,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    //TODO
+    res.render('gpangiLogin.html');
+});
+
 module.exports = router;
