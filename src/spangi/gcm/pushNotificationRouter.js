ee50a6eee3de616a3697ae78c2a251a12e2821b7
YeonwooSung <neos960518@gmail.com>
2020-06-30 14:16:02 +0900

    Minor update for push notification part

--- src/spangi/gcm/pushNotificationRouter.js
+++ src/spangi/gcm/pushNotificationRouter.js
@@ -3,17 +3,22 @@
 var express = require('express');
 var router = express.Router();
 
+//TODO
+var push_token = '';
 
 router.post('/', (req, res) => {
     var fcm_target_token = req.query.token;
     var device_id = req.query.deviceID;
+
+    //TODO
+    push_token = fcm_target_token;
 
     res.send('ok');
 });
 
 router.post('/send', (req, res) => {
     //TODO get data
-    var fcm_target_token = ''
+    var fcm_target_token = push_token;
     var title = 'TEST'
     var body = 'just a test message'
 
