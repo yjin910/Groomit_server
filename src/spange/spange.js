941dba291f09ed2f78524a15baaebda0544c085e
YeonwooSung <neos960518@gmail.com>
2020-07-15 14:43:56 +0900

    Add new endpoints

--- src/spange/spange.js
+++ src/spange/spange.js
@@ -27,6 +27,10 @@ router.post('/', (req, res) => {
     dbConn.registerUser_SPANGE(res, user_id, fcm_target_token);
 });
 
+router.post('/registerNewDevice', (req, res) => dbConn.registerDevice_SPANGE(res, req.query.deviceID) );
+
+router.post('/registerAdminUserForDevice', (req, res) => dbConn.registerAdminUserForDevice_SPANGE(res, req.query.userID, req.query.deviceID) );
+
 router.post('/updateToken', (req, res) => {
     var previous_token = req.query.previousToken;
     var new_token = req.query.newToken;
