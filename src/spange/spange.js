6f3d9b17d0813ec9cc840eace69550a699fed93c
YeonwooSung <neos960518@gmail.com>
2020-07-15 15:06:08 +0900

    Refactor endpoint functions

--- src/spange/spange.js
+++ src/spange/spange.js
@@ -6,15 +6,11 @@ var dbConn = require('../dbConnection');
 
 const API_CODE_SPACOSA_LOCATION = 'api.push.member.location';
 
+const DEFAULT_LATITUDE = 0;
+const DEFAULT_LONGITUDE = 0;
 
-router.get('/', (req, res) => {
-    //TODO location data
-    var latitude = '37.401782989502';
-    var longitude = '126.7320098877';
 
-    //sendPushNotification(fcm_target_token, title, body, latitude, longitude);
-    dbConn.getTokenByDeviceID_SPANGE(res, '61075', latitude, longitude);
-});
+router.get('/', (req, res) => dbConn.getTokenByDeviceID_SPANGE(res, '61075', '37.401782989502', '126.7320098877') );
 
 router.post('/', (req, res) => {
     var fcm_target_token = req.query.token;
@@ -31,26 +27,11 @@ router.post('/registerNewDevice', (req, res) => dbConn.registerDevice_SPANGE(res
 
 router.post('/registerAdminUserForDevice', (req, res) => dbConn.registerAdminUserForDevice_SPANGE(res, req.query.userID, req.query.deviceID) );
 
-router.post('/updateToken', (req, res) => {
-    var previous_token = req.query.previousToken;
-    var new_token = req.query.newToken;
+router.post('/updateToken', (req, res) => dbConn.updateGCMToken_SPANGE(res, req.query.previousToken, req.query.newToken) );
 
-    dbConn.updateGCMToken_SPANGE(res, previous_token, new_token);
-});
+router.post('/registerDevice', (req, res) => dbConn.registerDeviceID_SPANGE(res, req.query.userID, req.query.deviceID) );
 
-router.post('/registerDevice', (req, res) => {
-    var device_id = req.query.deviceID;
-    var user_id = req.query.userID;
-
-    dbConn.registerDeviceID_SPANGE(res, user_id, device_id);
-});
-
-router.post('/registerUser', (req, res) => {
-    var user_id = req.query.userID;
-    var token = req.query.token;
-
-    dbConn.registerUser_SPANGE(res, user_id, token);
-});
+router.post('/registerUser', (req, res) => dbConn.registerUser_SPANGE(res, req.query.userID, req.query.token) );
 
 router.get('/send', (req, res) => {
     console.log('req.query', req.query);
@@ -63,11 +44,13 @@ router.post('/send', (req, res) => {
     var {latitude, longitude} = req.body.data;
     var {api_code, partner_key, member_key, device_sn} = req.body.data;
 
-    if (api_code != API_CODE_SPACOSA_LOCATION) res.send('api_code mismatch!');
+    if (api_code != API_CODE_SPACOSA_LOCATION) {
+        res.send('api_code mismatch!');
+        return;
+    }
 
-    //TODO
-    if (latitude == null) latitude = 0;
-    if (longitude == null) longitude = 0;
+    if (latitude == null) latitude = DEFAULT_LATITUDE;
+    if (longitude == null) longitude = DEFAULT_LONGITUDE;
 
     dbConn.getTokenByDeviceID_SPANGE(res, device_sn, latitude, longitude);
 });
